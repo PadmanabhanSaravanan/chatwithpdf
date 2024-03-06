bcc789503d7895c9ce368cb4cb0f180fa61a3a5a
PadmanabhanSaravanan <g.s.r.padmanabhan@gmail.com>
2024-03-06 10:18:21 +0530

    added comments

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
+# This method takes a list of PDF files as input, reads them using PdfReader from PyPDF2, 
+# and concatenates the text content from all pages into a single string
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -18,7 +19,7 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
-
+# This method splits the provided text into smaller chunks. It uses the CharacterTextSplitter from langchain library
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
@@ -29,16 +30,14 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-
+# This method converts text chunks into embeddings and creates a FAISS vector store.
 def get_vectorstore(text_chunks):
-    # embeddings = OpenAIEmbeddings()
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
-
+# This method initializes a conversational retrieval chain using a language model and a vector store
 def get_conversation_chain(vectorstore):
-    # llm = ChatOpenAI()
     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
@@ -50,7 +49,7 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
-
+# This method handles user input by passing the question to the conversation chain and displaying the response
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
@@ -63,7 +62,8 @@ def handle_userinput(user_question):
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
 
-
+#  This is the main function which orchestrates the entire process. It sets up 
+# the Streamlit app, handles user input, and processes PDF documents.
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs",
